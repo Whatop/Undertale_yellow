2e050f77c572d07afd3f387c795929eafaea8961
Whatop <73518664+Whatop@users.noreply.github.com>
2024-01-17 22:06:16 +0900

    Camera
    MouseZoom

---
+++ Framework/Assets/Script/Camera/CameraZoom.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float zoomSpeed = 5f; // 조절할 확대 속도
+    public float minZoom = 2f;   // 최소 크기
+    public float maxZoom = 5f;  // 최대 크기
+
+    void Update()
+    {
+        float scrollWheelInput = Input.GetAxis("Mouse ScrollWheel");
+
+        // 마우스 휠 입력이 감지되면
+        if (scrollWheelInput != 0)
+        {
+            // 카메라 확대/축소를 위한 새로운 카메라 크기 계산
+            float newCameraSize = Camera.main.orthographicSize - scrollWheelInput * zoomSpeed;
+
+            // 새로운 카메라 크기를 최소 크기와 최대 크기 사이로 제한
+            newCameraSize = Mathf.Clamp(newCameraSize, minZoom, maxZoom);
+
+            // 카메라 크기를 업데이트
+            Camera.main.orthographicSize = newCameraSize;
+        }
+    }
+}
