2cad0fac488f09c5ecb793564a4df94c18e9ef04
Whatop <73518664+Whatop@users.noreply.github.com>
2024-02-20 21:43:23 +0900

    구르기 애니메이션
    코드 만드는중

--- Framework/Assets/Script/PlayerMovement.cs
+++ Framework/Assets/Script/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,7 +19,11 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rigid;
     public float h;
     public float v;
-    Vector3 dirVec;
+
+    bool isMove = false;
+
+    public float cooldownTime = 0.5f;
+    private bool isCooldown = false;
 
     GameObject scanObject;
     public PlayerState playerState;
@@ -37,54 +42,175 @@ public class PlayerMovement : MonoBehaviour
 
 
         Debug.Log("현재 각도: " + angle);
+        if (Input.GetMouseButtonDown(1) && !isCooldown && isMove)
+        {
+            // 우클릭 입력이 감지되면 처리하고 쿨타임 시작
+            playerState = PlayerState.Roll;
+            StartRoll();
+            StartCooldown();
+        }
 
+        // 쿨타임 감소
+        if (isCooldown)
+        {
+            cooldownTime -= Time.deltaTime;
 
-        animator.SetBool("IsUp", false);
-        animator.SetBool("IsSide", false);
-        animator.SetBool("IsDown", false);
-        animator.SetBool("IsAngle", false);
+            // 쿨타임이 끝나면 상태 초기화
+            if (cooldownTime <= 0)
+            {
+                isCooldown = false;
+                cooldownTime = 0.5f; // 원하는 쿨타임 값으로 설정
+                playerState = PlayerState.None;
+            }
+        }
 
-        if (angle > -45f && angle <= 15f)
+        if (playerState != PlayerState.Roll)//구르기는 각도처리를 따로해서
+        {
+            animator.SetBool("IsUp", false);
+            animator.SetBool("IsSide", false);
+            animator.SetBool("IsDown", false);
+            animator.SetBool("IsAngle", false);
+
+            if (angle > -45f && angle <= 15f)
+            {
+                Vector3 currentScale = transform.localScale;
+                currentScale.x = Mathf.Abs(currentScale.x) * 1;
+                transform.localScale = currentScale;
+
+                SetPlayerState(PlayerState.Side);
+            }
+            else if (angle > 45f && angle <= 135f)
+            {
+                SetPlayerState(PlayerState.Up);
+            }
+            else if ((angle > 165f && angle <= 180f) || (angle >= -180f && angle < -135f))
+            {
+                Vector3 currentScale = transform.localScale;
+                currentScale.x = Mathf.Abs(currentScale.x) * -1;
+                transform.localScale = currentScale;
+                SetPlayerState(PlayerState.Side);
+            }
+            else if (angle >= -135f && angle < -45f)
+            {
+                SetPlayerState(PlayerState.Down);
+            }
+            else if (angle < 45f && angle >= 15f)
+            {
+                Vector3 currentScale = transform.localScale;
+                currentScale.x = Mathf.Abs(currentScale.x) * 1;
+                transform.localScale = currentScale;
+
+                SetPlayerState(PlayerState.Angle);
+            }
+            else
+            {
+                Vector3 currentScale = transform.localScale;
+                currentScale.x = Mathf.Abs(currentScale.x) * -1;
+                transform.localScale = currentScale;
+
+                SetPlayerState(PlayerState.Angle);
+            }
+        }
+    }
+
+    private void StartRoll() //Roll은 각도처리 따로 함 이동값으로 판별해서 
+    {
+        if (v != 0 && h == 0) // 왼쪽 또는 오른쪽으로 이동할 때
         {
             Vector3 currentScale = transform.localScale;
-            currentScale.x = Mathf.Abs(currentScale.x) * 1;
+            if (h < 0) // 왼쪽 이동
+            {
+                currentScale.x = Mathf.Abs(currentScale.x) * -1;// 스케일 반대로 설정
+            }
+            else // 오른쪽 이동
+            {
+                currentScale.x = Mathf.Abs(currentScale.x) * 1;//  스케일 그대로 설정
+            }
             transform.localScale = currentScale;
-
-            SetPlayerState(PlayerState.Side);
+            animator.SetBool("IsSide", true);
         }
-        else if (angle > 45f && angle <= 135f)
+        else if (v == 0 && h != 0) // 위 또는 아래로 이동할 때
         {
-            SetPlayerState(PlayerState.Up);
+            animator.SetBool("IsUp", v > 0);
+            animator.SetBool("IsDown", v < 0);
         }
-        else if ((angle > 165f && angle <= 180f) || (angle >= -180f && angle < -135f))
+        else // 대각선 이동일 때
         {
             Vector3 currentScale = transform.localScale;
-            currentScale.x = Mathf.Abs(currentScale.x) * -1;
+            if (h < 0 && v > 0) // 왼쪽 위 대각선
+            {
+                currentScale.x = Mathf.Abs(currentScale.x) * -1;// 스케일 반대로 설정
+                animator.SetBool("IsAngle", true);
+            }
+            else if (h > 0 && v > 0) // 오른쪽 위 대각선
+            {
+                currentScale.x = Mathf.Abs(currentScale.x) * 1;//  스케일 그대로 설정
+                animator.SetBool("IsAngle", true);
+            }
+            else if (h < 0 && v < 0) // 왼쪽 아래 대각선
+            {
+                animator.SetBool("IsSide", true);
+                currentScale.x = Mathf.Abs(currentScale.x) * -1;// 스케일 반대로 설정
+            }
+            else if (h > 0 && v < 0) // 오른쪽 아래 대각선
+            {
+                animator.SetBool("IsSide", true);
+                currentScale.x = Mathf.Abs(currentScale.x) * 1;//  스케일 그대로 설정
+            }
             transform.localScale = currentScale;
 
-            SetPlayerState(PlayerState.Side);
+            //왼쪽 대각선 아래면 스케일반대로, 오른쪽 아래 대각선이면 그대로 
         }
-        else if (angle >= -135f && angle < -45f)
-        {
-            SetPlayerState(PlayerState.Down);
-        }
-        else if(angle < 45f && angle >= 15f)
-        {
-            Vector3 currentScale = transform.localScale;
-            currentScale.x = Mathf.Abs(currentScale.x) * 1;
-            transform.localScale = currentScale;
+            animator.SetTrigger("IsRoll");
+    }
 
-            SetPlayerState(PlayerState.Angle);
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        if (h != animator.GetInteger("h"))
+        {
+            isMove = true;
+            animator.SetInteger("h", (int)h);
+
+        }
+        else if (v != animator.GetInteger("v"))
+        {
+            isMove = true;
+            animator.SetInteger("v", (int)v);
+        }
+
+        if (v == 0 && h == 0)
+        {
+            isMove = false;
+        }
+        animator.SetBool("isMove", isMove);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            MoveSpeed = 6f;
         }
         else
         {
-            Vector3 currentScale = transform.localScale;
-            currentScale.x = Mathf.Abs(currentScale.x) * -1;
-            transform.localScale = currentScale;
-
-            SetPlayerState(PlayerState.Angle);
+            MoveSpeed = 4f;
         }
-
+        if (gameManager.GetPlayerData().currentState == GameState.Event)
+        {
+            animator.SetInteger("v", 0);
+            animator.SetInteger("h", 0);
+            MoveSpeed = 0f;
+        }
+        rigid.velocity = new Vector2(h, v) * MoveSpeed;
+    }
+    void StartCooldown()
+    {
+        // 쿨타임 시작
+        isCooldown = true;
     }
     void SetPlayerState(PlayerState newState)
     {
@@ -112,7 +238,6 @@ public class PlayerMovement : MonoBehaviour
                 break;
         }
     }
-
     float CalculateMouseAngle()
     {
         Vector3 mousePosition = Input.mousePosition;
@@ -120,48 +245,6 @@ public class PlayerMovement : MonoBehaviour
         Vector3 targetPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         Vector3 direction = targetPosition - transform.position;
         return Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-    }
-    void FixedUpdate()
-    {
-        Move();
-    }
-    void Move()
-    {
-        h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("Vertical");
-
-        if (h != animator.GetInteger("h"))
-        {
-            animator.SetInteger("h", (int)h);
-
-            animator.SetBool("isMove", true);
-        }
-        else if (v != animator.GetInteger("v"))
-        {
-            animator.SetInteger("v", (int)v);
-
-            animator.SetBool("isMove", true);
-        }
-
-        if (v == 0 && h == 0)
-        {
-            animator.SetBool("isMove", false);
-        }
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            MoveSpeed = 6f;
-        }
-        else
-        {
-            MoveSpeed = 4f;
-        }
-        if (gameManager.GetPlayerData().currentState == GameState.Event)
-        {
-            animator.SetInteger("v", 0);
-            animator.SetInteger("h", 0);
-            MoveSpeed = 0f;
-        }
-        rigid.velocity = new Vector2(h, v) * MoveSpeed;
     }
     void UpdateAnimation(Vector2 moveInput)
     {
