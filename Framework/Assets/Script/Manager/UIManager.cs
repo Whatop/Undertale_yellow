412e2781eff3a978e6b663e25f28df9e82576674
Whatop <73518664+Whatop@users.noreply.github.com>
2024-03-21 12:21:10 +0900

    sah

---
+++ Framework/Assets/Script/Manager/UIManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<UIManager>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject("UIManager");
+                    instance = obj.AddComponent<UIManager>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
